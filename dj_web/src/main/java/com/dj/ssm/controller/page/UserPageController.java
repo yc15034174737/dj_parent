3c9143e9a1f3215188a78192527ebf315d2d62dc
yc15034174737 <68263089+yc15034174737@users.noreply.github.com>
2020-07-16 11:46:44 +0800

    Update UserPageController.java
    aa

--- dj_web/src/main/java/com/dj/ssm/controller/page/UserPageController.java
+++ dj_web/src/main/java/com/dj/ssm/controller/page/UserPageController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user/")
 public class UserPageController {
 
+    private String a;
     @Autowired
     private UserService userService;
 
